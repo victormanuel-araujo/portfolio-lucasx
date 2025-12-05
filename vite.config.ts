3c188e616c58402e8e03d8f0f6e48c1dc1b4f38b
Victor Araujo <victormanuel.araujo@outlook.com>
2025-12-04 21:09:42 -0300

    feat: added metadata

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: ["117191ea1177.ngrok-free.app"],
+    allowedHosts: ["db1a19109754.ngrok-free.app"],
   },
 });
